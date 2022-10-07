a6f17df256bfdeba486e9b6d61fc1babaf8ecef4
Shyam-Chen <shyamchen1994@gmail.com>
2022-10-07 17:11:43 +0800

    27th Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,20 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import fastify from './vite-plugin-fastify';
 import envify from 'process-envify';
 
 import env from './env';
 
 export default defineConfig({
   define: envify(env),
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    fastify({
-      appPath: './src/app.ts',
-      serverPath: './src/server.ts',
-      adapter: 'fastify',
-    }),
-  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
     },
   },
-  test: {},
+  build: {
+    rollupOptions: {
+      output: [{ format: 'cjs' }, { format: 'es' }],
+    },
+    ssr: './src/server.ts',
+  },
 });
