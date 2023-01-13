383ae5651065555c1e3fd9e6cd1902bbf2e9c34f
Shyam-Chen <shyamchen1994@gmail.com>
2023-01-13 11:33:56 +0800

    129th Commit

--- src/composables/useMailer.ts
+++ src/composables/useMailer.ts
@@ -1,5 +1,14 @@
 import nodemailer from 'nodemailer';
 
+/**
+ * Google Account -> Security -> App passwords (Signing in to Google)
+ *  -> Select app -> Other (Custom name)
+ *  -> GENERATE
+ *
+ * smtps://__EMAIL_ADDRESS__:__APP_PASSWORD__@smtp.gmail.com:465?name=__EMAIL_ADDRESS__
+ */
+const transport = nodemailer.createTransport({ url: process.env.SMTP_URL });
+
 export default () => {
-  return nodemailer.createTransport({ url: process.env.SMTP_URL });
+  return transport;
 };
