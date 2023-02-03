79578af00ff137b69aad79f99995c2779c901493
Shyam-Chen <shyamchen1994@gmail.com>
2023-02-03 15:44:31 +0800

    145th Commit

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,16 @@ const start = async () => {
     server.log.error(err);
     process.exit(1);
   }
+
+  if (import.meta.hot) {
+    import.meta.hot.on('vite:beforeFullReload', () => {
+      server.close();
+    });
+
+    import.meta.hot.dispose(() => {
+      server.close();
+    });
+  }
 };
 
 start();
