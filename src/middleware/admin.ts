b561a4ee9828cc97f0029ca551645b2d8e9988f4
Shyam-Chen <shyamchen1994@gmail.com>
2023-02-04 09:33:10 +0800

    146th Commit

---
+++ src/middleware/admin.ts
@@ -0,0 +1,20 @@
+import type { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+
+export default (app: FastifyInstance) => {
+  return async <T extends FastifyRequest, U extends FastifyReply>(request: T, reply: U) => {
+    const users = app.mongo.db?.collection('users');
+    const roles = app.mongo.db?.collection('roles');
+
+    const user = await users?.findOne(
+      { username: { $eq: request.user.username } },
+      { projection: { password: 0, secret: 0 } },
+    );
+
+    const role = await roles?.findOne(
+      { userId: { $ref: 'users', $id: user?._id } },
+      { projection: { role: 1, permissions: 1 } },
+    );
+
+    if (role?.role !== 'admin') return reply.notFound();
+  };
+};
