6034c8cb32c29a242f2ae7967d443f3312d646df
Shyam-Chen <shyamchen1994@gmail.com>
2023-12-01 12:16:51 +0800

    227th Commit

--- src/routes/file-uploads/import-data/+handler.ts
+++ src/routes/file-uploads/import-data/+handler.ts
@@ -1,16 +1,29 @@
 import type { FastifyInstance } from 'fastify';
 import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
-
-import { read } from 'xlsx';
+import type { Row } from 'exceljs';
+import { Workbook } from 'exceljs';
 
 export default async (app: FastifyInstance) => {
   const router = app.withTypeProvider<TypeBoxTypeProvider>();
 
   router.post('', async (req, reply) => {
     const data = await req.file();
-    const buffer = await data?.toBuffer();
-    const workbook = read(buffer);
 
-    return reply.send({ message: 'OK', sheets: workbook.Sheets });
+    if (!data) return reply.badRequest();
+
+    const workbook = new Workbook();
+    const xlsx = await workbook.xlsx.read(data.file);
+
+    const worksheet = xlsx.worksheets[0];
+
+    const result = [] as Row['values'][];
+
+    worksheet.eachRow(function (row, rowNumber) {
+      if (rowNumber > 1) {
+        result.push(row.values);
+      }
+    });
+
+    return reply.send({ message: 'OK', result });
   });
 };
