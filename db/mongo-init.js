324d4f051f45748f258aef5053ef0677bbc60ae9
Shyam-Chen <shyamchen1994@gmail.com>
2024-02-18 21:22:56 +0800

    260th Commit

--- db/mongo-init.js
+++ db/mongo-init.js
@@ -16,4 +16,27 @@ db.createUser({
 
 db = new Mongo().getDB(process.env.MONGO_INITDB_DATABASE);
 
-load('./src/todos.js');
+// load('./src/todos.js');
+
+db.createCollection('todos');
+
+db.todos.insertMany([
+  {
+    title: 'Node.js',
+    completed: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+  {
+    title: 'Fastify',
+    completed: true,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+  {
+    title: 'MongoDB',
+    completed: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+]);
--- db/src/todos.js
+++ db/src/todos.js
@@ -1,13 +1,24 @@
+// db = connect('mongodb://root:rootpasswd@127.0.0.1:27017/mydb?retryWrites=true&w=majority');
+
 db.createCollection('todos');
 
-db.insertMany([
+db.todos.insertMany([
   {
     title: 'Node.js',
+    completed: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
   },
   {
     title: 'Fastify',
+    completed: true,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
   },
   {
     title: 'MongoDB',
+    completed: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
   },
 ]);
