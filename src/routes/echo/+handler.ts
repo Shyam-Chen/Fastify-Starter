1e266ccdce138d07eb5288bd7ccb8cf3f07b622f
Shyam-Chen <shyamchen1994@gmail.com>
2024-02-26 11:28:21 +0800

    273rd Commit

--- src/routes/echo/+handler.ts
+++ src/routes/echo/+handler.ts
@@ -2,16 +2,15 @@ import type { FastifyInstance } from 'fastify';
 
 export default async (app: FastifyInstance) => {
   app.get('', { websocket: true }, (con) => {
-    console.log('Client connected');
-
-    con.socket.send(`Hello from Fastify!`);
+    app.log.info('Client connected');
 
     con.socket.on('message', (message: MessageEvent) => {
       console.log(`Client message: ${message}`);
+      con.socket.send(`Hello from Fastify!`);
     });
 
     con.socket.on('close', () => {
-      console.log('Client disconnected');
+      app.log.info('Client disconnected');
     });
   });
 };
---
+++ src/routes/echo/__tests__/+handler.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from 'vitest';
+import fastify from 'fastify';
+import websocket from '@fastify/websocket';
+
+import echo from '../+handler';
+
+test('WS /echo', async () => {
+  const app = fastify();
+
+  app.register(websocket);
+  app.register(echo, { prefix: '/echo' });
+
+  await app.ready();
+  const ws = await app.injectWS('/echo');
+
+  let resolve: (value: string) => void;
+  const promise = new Promise<string>((_resolve) => (resolve = _resolve));
+
+  ws.on('message', (data: MessageEvent) => {
+    resolve(data.toString());
+  });
+
+  ws.send('Hi from Test!');
+
+  expect(await promise).toEqual('Hello from Fastify!');
+
+  ws.terminate();
+});
