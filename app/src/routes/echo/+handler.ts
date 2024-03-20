e0d5d20e21405d01dbb2838044906ab67439bc76
Shyam-Chen <shyamchen1994@gmail.com>
2024-03-20 15:07:57 +0800

    292nd Commit

--- app/src/routes/echo/+handler.ts
+++ app/src/routes/echo/+handler.ts
@@ -1,15 +1,15 @@
 import type { FastifyInstance } from 'fastify';
 
 export default async (app: FastifyInstance) => {
-  app.get('', { websocket: true }, (con) => {
+  app.get('', { websocket: true }, (socket) => {
     app.log.info('Client connected');
 
-    con.socket.on('message', (message: MessageEvent) => {
+    socket.on('message', (message: MessageEvent) => {
       console.log(`Client message: ${message}`);
-      con.socket.send('Hello from Fastify!');
+      socket.send('Hello from Fastify!');
     });
 
-    con.socket.on('close', () => {
+    socket.on('close', () => {
       app.log.info('Client disconnected');
     });
   });
