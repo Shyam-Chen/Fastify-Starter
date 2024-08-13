8c5dc6615ee3c7dc97295c0591a2491fe3d9f2e6
Shyam-Chen <shyamchen1994@gmail.com>
2024-08-13 14:06:36 +0800

    369th Commit

--- app/src/routes/file-uploads/+handler.ts
+++ app/src/routes/file-uploads/+handler.ts
@@ -29,23 +29,34 @@ export default (async (app) => {
       if (!data) return reply.badRequest();
 
       const public_id = path.basename(data.filename, path.extname(data.filename));
+      const folder = data.mimetype.includes('image') ? 'images' : undefined;
 
       if (process.env.NODE_ENV === 'production') {
-        await pipeline(data.file, app.cloudinary.uploader.upload_stream({ public_id }));
+        await pipeline(data.file, app.cloudinary.uploader.upload_stream({ public_id, folder }));
       } else {
         const dir = path.resolve(import.meta.dirname, '../../../dist');
         if (!fs.existsSync(dir)) fs.mkdirSync(dir);
         await pipeline(data.file, fs.createWriteStream(path.resolve(dir, data.filename)));
       }
 
-      return reply.send({ message: 'OK', url: app.cloudinary.url(public_id) });
+      return reply.send({
+        message: 'OK',
+        url: app.cloudinary.url(folder ? `${folder}/${public_id}` : public_id),
+      });
     },
   );
 
+  /*
+  $ curl --request GET \
+         --url http://127.0.0.1:3000/api/file-uploads?publicId=images/avatar
+  */
   app.get(
     '',
     {
       schema: {
+        querystring: Type.Object({
+          publicId: Type.String(),
+        }),
         response: {
           '2xx': {
             message: Type.String(),
@@ -55,7 +66,7 @@ export default (async (app) => {
       },
     },
     async (req, reply) => {
-      return reply.send({ message: 'OK', url: app.cloudinary.url('userfile') });
+      return reply.send({ message: 'OK', url: app.cloudinary.url(req.query.publicId) });
     },
   );
 }) as FastifyPluginAsyncTypebox;
