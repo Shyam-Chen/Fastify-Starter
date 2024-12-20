93ea0a5b41dfd24a4f6629918697d189dda559c7
Shyam-Chen <shyamchen1994@gmail.com>
2024-12-20 18:14:03 +0800

    416th Commit

--- app/src/routes/users/schema.ts
+++ app/src/routes/users/schema.ts
@@ -5,18 +5,17 @@ export const UserBox = Type.Object({
   username: Type.String(),
   email: Type.String({ format: 'email' }),
   fullName: Type.String(),
-  role: Type.Union([
-    Type.Literal('viewer'),
-    Type.Literal('editor'),
-    Type.Literal('admin'),
-    Type.Literal('custom'),
-  ]),
   status: Type.Boolean(),
   otpEnabled: Type.Boolean(),
   otpVerified: Type.Boolean(),
 });
 
 export const RoleBox = Type.Object({
-  role: Type.String(),
+  role: Type.Union([
+    Type.Literal('viewer'),
+    Type.Literal('editor'),
+    Type.Literal('admin'),
+    Type.Literal('custom'),
+  ]),
   permissions: Type.Array(Type.Any()),
 });
